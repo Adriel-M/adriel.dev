55c8bec4964155d9d242207a27683bfee6c56bd6
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2025-05-30 02:53:10 -0500

    Use Astro Picture for images (#13)

--- src/libs/ComponentOverrides.ts
+++ src/libs/ComponentOverrides.ts
@@ -1,7 +1,9 @@
 import Link from '@/components/Link.astro'
+import Picture from '@/components/Picture.astro'
 import TableWrapper from '@/components/TableWrapper.astro'
 
 export default {
   a: Link,
   table: TableWrapper,
+  img: Picture,
 }
