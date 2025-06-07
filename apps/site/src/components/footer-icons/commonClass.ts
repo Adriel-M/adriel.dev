c68f440d4db90d1ad42c4cd95c39e19dd48dfdcc
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2025-06-07 19:12:24 -0400

    some cleanup (#89)

--- apps/site/src/components/footer-icons/commonClass.ts
+++
@@ -1,1 +0,0 @@
-export const classes = 'fill-current text-gray-700 hover:text-primary-400 size-6'
--- apps/site/src/libs/ComponentOverrides.ts
+++ apps/site/src/libs/ComponentOverrides.ts
@@ -1,11 +1,11 @@
 import Gallery from '@/components/Gallery.astro'
 import Link from '@/components/Link.astro'
-import Picture from '@/components/Picture.astro'
+import ResponsivePicture from '@/components/ResponsivePicture.astro'
 import TableWrapper from '@/components/TableWrapper.astro'
 
 export default {
   a: Link,
   table: TableWrapper,
-  img: Picture,
+  img: ResponsivePicture,
   Gallery,
 }
