ddb146ad4dc2a4c97be192822ec97f30f58c2f66
Adriel Martinez <20115135+Adriel-M@users.noreply.github.com>
2026-04-04 22:13:30 -0700

    Remove projects page and functionality (#301)

--- apps/site/src/libs/headerNavLinks.ts
+++ apps/site/src/libs/headerNavLinks.ts
@@ -3,7 +3,6 @@ import { URLS } from '@/libs/UrlLibs'
 const headerNavLinks = [
   { href: URLS.HOME, title: 'Home' },
   { href: URLS.POSTS, title: 'Posts' },
-  { href: URLS.PROJECTS, title: 'Projects' },
   { href: URLS.ABOUT, title: 'About' },
 ]
 
--- apps/site/src/pages/projects/og.png.ts
+++
@@ -1,4 +0,0 @@
-import { generateGet } from '@/libs/generateOgGet.tsx'
-import { title } from '@/pages/projects/index.astro'
-
-export const { GET } = generateGet(title)
